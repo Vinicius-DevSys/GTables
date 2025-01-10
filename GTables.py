bfe28c00cda0c84e5dfddd06cdeecf6695dfea75
Vinicius-DevSys <vinicius.dev.business@gmail.com>
2025-01-09 22:38:08 -0300

    create_progress_data - Método para geração de números progressivos adicionado + mudança de nome dos métodos

--- GTables.py
+++ GTables.py
@@ -14,16 +14,21 @@ class TableMaker():
         return self.table
     
     def build(self):
-        with open("Table.csv", "w", newline="") as file:
+        with open("Table.csv", "w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
             for row in self.table:
                 writer.writerow(row)
 
 class DataMaker():
-    def __init__(self, substantive, adjective):
+    def __init__(self, substantive, adjective, last_number=0):
         self.substantive = substantive
         self.adjective = adjective
+        self.last_number = last_number
         
-    def build_data(self):
+    def create_name_data(self):
         return f"{random.choice(self.substantive)} {random.choice(self.adjective)}"
 
+    def create_progress_data(self, minmax=[0, 10]):
+        send_number = self.last_number
+        self.last_number += random.randint(minmax[0], minmax[1])
+        return send_number
