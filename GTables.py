86de1ef895a5f95c72accb09bd5b7e931478674d
Vinicius-DevSys <vinicius.dev.business@gmail.com>
2025-01-09 22:34:50 -0300

    Modelo inicial do objeto DataMaker estabelecido.

--- GTables.py
+++ GTables.py
@@ -1,4 +1,4 @@
-import csv
+import csv, random
 
 class TableMaker():
     def __init__(self, columns):
@@ -18,3 +18,12 @@ class TableMaker():
             writer = csv.writer(file)
             for row in self.table:
                 writer.writerow(row)
+
+class DataMaker():
+    def __init__(self, substantive, adjective):
+        self.substantive = substantive
+        self.adjective = adjective
+        
+    def build_data(self):
+        return f"{random.choice(self.substantive)} {random.choice(self.adjective)}"
+
